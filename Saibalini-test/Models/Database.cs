991aca1ddacf4d5a3dd97f6f7945b2a9c1cc4bf7
Saibalini Ray <Saibalini.Ray@gds.ey.com>
2024-07-03 14:46:55 +0530

    security change

--- Saibalini-test/Models/Database.cs
+++ Saibalini-test/Models/Database.cs
@@ -9,8 +9,6 @@ namespace Saibalini_test.Models
             : base(options)
         { }
         public DbSet<Product> Products => Set<Product>();
-        public DbSet<User> Users => Set<User>();
-        public DbSet<Role> Roles => Set<Role>();
 
     }
     public class Product
@@ -19,19 +17,5 @@ namespace Saibalini_test.Models
         public required string Name { get; set; }
         public decimal Price { get; set; }
         public string? Description { get; set; }
-    }
-    public class User
-    {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-        public Role? Role { get; set; }
-    }
-    public class Role
-    {
-        public int RoleId { get; set; }
-        public required string Name { get; set; }
-        public IEnumerable<User>? Users { get; set; }
     }
 }
